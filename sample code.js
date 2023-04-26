01f3b49d6d390d9448bd7d78e8ac04ca14dd1e22
trife631 <126350708+trife631@users.noreply.github.com>
2023-04-26 18:12:41 -0400

    logo maker first commits

---
+++ sample code.js
@@ -0,0 +1,10 @@
+// SUGGESTED DIRECTORY STRUCTURE
+// ├── examples/           // Example svg file(s) created with the app
+// ├── lib/                // Folder for classes or functions
+//     ├── shapes.js       // Exports `Triangle`, `Circle`, and `Square` classes
+//     ├── shapes.test.js  // Jest tests for shapes
+//     └── more...         // Additional files and tests
+// ├── .gitignore          // Indicates which folders and files Git should ignore
+// ├── index.js            // Runs the application using imports from lib/
+// ├── package.json
+// └── README.md           // App description, link to video, setup and usage instructions           
